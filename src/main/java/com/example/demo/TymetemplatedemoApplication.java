10f5ff72291a3ffdb95f2213db193df256216b1e
K <nonsense@gmail.com>
2018-04-12 19:24:41 -0500

    Third app alone

---
+++ src/main/java/com/example/demo/TymetemplatedemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@Controller
+public class TymetemplatedemoApplication {
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("Author", "Kate");
+		model.addAttribute("fileName", "Chocolat");
+
+		List favouriteFoods = Arrays.asList("Noodles", "Burritos", "Ice cream");
+		model.addAttribute("favouriteFoods", favouriteFoods);
+		return "index.html";
+	}
+		public static void main(String[] args) {
+		SpringApplication.run(TymetemplatedemoApplication.class, args);
+	}
+}
